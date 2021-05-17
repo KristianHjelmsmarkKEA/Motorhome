6300f40b148bfa38880b3e5b0e6b8005c731710b
Ludvig <ludvig.fohns@gmail.com>
2021-05-17 20:27:21 +0200

    contructors, getters&setters

--- src/main/java/com/example/demo/Model/Contract.java
+++ src/main/java/com/example/demo/Model/Contract.java
@@ -4,10 +4,54 @@ import java.util.Date;
 
 public class Contract {
 
+    public Contract(){
+
+    }
+
+
     private int contractID;
     private Date startDate;
     private Date endDate;
     private boolean finalizedContract;
     private boolean cancelledContract;
 
+    public int getContractID() {
+        return contractID;
+    }
+
+    public void setContractID(int contractID) {
+        this.contractID = contractID;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isFinalizedContract() {
+        return finalizedContract;
+    }
+
+    public void setFinalizedContract(boolean finalizedContract) {
+        this.finalizedContract = finalizedContract;
+    }
+
+    public boolean isCancelledContract() {
+        return cancelledContract;
+    }
+
+    public void setCancelledContract(boolean cancelledContract) {
+        this.cancelledContract = cancelledContract;
+    }
 }
--- src/main/java/com/example/demo/Model/Motorhome.java
+++ src/main/java/com/example/demo/Model/Motorhome.java
@@ -2,6 +2,10 @@ package com.example.demo.Model;
 
 public class Motorhome {
 
+    public Motorhome(){
+
+    }
+
     private int motorhomeID;
     private String brandAndModel;
     private int capacity;
@@ -10,5 +14,59 @@ public class Motorhome {
     private String plateNumber;
     private int rentalPrice;
 
+    public int getMotorhomeID() {
+        return motorhomeID;
+    }
 
+    public void setMotorhomeID(int motorhomeID) {
+        this.motorhomeID = motorhomeID;
+    }
+
+    public String getBrandAndModel() {
+        return brandAndModel;
+    }
+
+    public void setBrandAndModel(String brandAndModel) {
+        this.brandAndModel = brandAndModel;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getOdometer() {
+        return odometer;
+    }
+
+    public void setOdometer(int odometer) {
+        this.odometer = odometer;
+    }
+
+    public boolean isService() {
+        return service;
+    }
+
+    public void setService(boolean service) {
+        this.service = service;
+    }
+
+    public String getPlateNumber() {
+        return plateNumber;
+    }
+
+    public void setPlateNumber(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    public int getRentalPrice() {
+        return rentalPrice;
+    }
+
+    public void setRentalPrice(int rentalPrice) {
+        this.rentalPrice = rentalPrice;
+    }
 }
