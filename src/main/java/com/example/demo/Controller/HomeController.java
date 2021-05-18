c114ce89ab15e02135fb7213efc49a2b3dfa70ec
Kristian097 <73465821+Kristian097@users.noreply.github.com>
2021-05-18 15:56:36 +0200

    Customer table search function & Homecontroller connected

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Model.Customer;
+import com.example.demo.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
     @Autowired
+    CustomerService customerService;
 
     /** Homepage */
     @GetMapping("/")
@@ -37,7 +42,8 @@ public class HomeController {
 
     @GetMapping ("/manageCustomers")
     public String manageCustomers(Model model) {
-        //Code ...
+        List<Customer> customerList = customerService.fetchAll();
+        model.addAttribute("customers", customerList);
         return "home/manageCustomers";
     }
 
