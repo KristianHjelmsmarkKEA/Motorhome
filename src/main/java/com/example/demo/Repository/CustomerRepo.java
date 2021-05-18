332379807f31086912ca8a49c9b0a757b7e6296b
Ludvig <ludvig.fohns@gmail.com>
2021-05-18 11:51:13 +0200

    Diverse methods til SQL query

--- src/main/java/com/example/demo/Repository/CustomerRepo.java
+++ src/main/java/com/example/demo/Repository/CustomerRepo.java
@@ -1,13 +1,51 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CustomerRepo {
 
     @Autowired
     JdbcTemplate template;
+
+    public List<Customer> fetchAll() {
+        String sql = "SELECT * FROM customers;";
+        RowMapper<Customer> rowMapper = new BeanPropertyRowMapper<>(Customer.class);
+        return template.query(sql, rowMapper);
+    }
+
+    public Customer addCountry(Customer customer){
+        String sql = "INSERT INTO country (country) VALUES (?)";
+        template.update(sql, customer.getCountry());
+        return null;
+    }
+    //SQL STRING SKAL ÆNDRES?
+    public Customer addZipcode(Customer customer) {
+        String sql = "INSERT INTO zipcodes (zipcode, city) VALUES (?, ?)";
+        template.update(sql, customer.getZipcode(), customer.getCity());
+        return null;
+    }
+
+    //SQL STRING SKAL ÆNDRES
+    public Customer addAddress(Customer customer) {
+        String sql = " INSERT INTO address (address, zipcodeida) VALUES (?, (select zipcodeid from zipcodes where zipcodeid = (select max(zipcodeid) from zipcodes)))";
+        template.update(sql, customer.getAddress());
+        return null;
+    }
+    //SQL STRING SKAL ÆNDRES
+    public Customer addCustomer(Customer customer) {
+        String sql = "INSERT INTO customers (firstname, mobile_phone, email, driver_licence, driver_since_date, addressidc) VALUES (?, ?, ?, ?, ?, (select addressid from address where addressid = (select max(addressid) from address)))";
+        template.update(sql, customer.getFirstName());
+        return null;
+    }
+
+
 
 }
--- src/main/java/com/example/demo/Repository/MotorhomeRepo.java
+++ src/main/java/com/example/demo/Repository/MotorhomeRepo.java
@@ -1,12 +1,31 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.Contract;
+import com.example.demo.Model.Motorhome;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class MotorhomeRepo {
 
     @Autowired
     JdbcTemplate template;
+
+    public List<Motorhome> fetchAll() {
+        String sql = "SELECT * FROM motorhomes";
+        RowMapper<Motorhome> rowMapper = new BeanPropertyRowMapper<>(Motorhome.class);
+        return template.query(sql, rowMapper);
+    }
+
+    //SQL STRING SKAL ÆNDRES
+    public Contract addMotorhome(Motorhome motorhome){
+        String sql = "INSERT INTO motorhomes (VALUES (?, ?, ?, ?, ?, ?)";
+        template.update(sql, motorhome.getBrandAndModel());
+        return null;
+    }
 }
