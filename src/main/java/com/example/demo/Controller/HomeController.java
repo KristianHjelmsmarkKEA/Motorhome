0ca9ae4453a8abdde1e8b4635a3aabbf7983b207
Gustav <gullegustav@gmail.com>
2021-05-18 12:04:22 +0200

    It works i guess

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
     @Autowired
 
-
+@RequestMapping("home/index")
+    public ModelAndView firstPage() {
+        return new ModelAndView("home/index");
+    }
 
     /** Homepage */
     @GetMapping("/")
